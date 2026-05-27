be1bbb11c15b69887342400c4d5ace107784baf5
Nadja Brix Koch <n.koch@samsung.com>
2026-05-27 10:53:25 +0200

    fix(headers): add include guards to headers
    Signed-off-by: Nadja Brix Koch <n.koch@samsung.com>

--- include/homid.h
+++ include/homid.h
@@ -1,5 +1,10 @@
+#ifndef HOMID_H
+#define HOMID_H
+
 struct homid {
   struct homid_ipc_connection *conn;
   struct homid_device *dev;	///< Pointer to array of 'struct homid_device'
   unsigned int ndevs;		///< Number of devices
 };
+
+#endif /* HOMID_H */
--- include/homid_log.h
+++ include/homid_log.h
@@ -1,3 +1,6 @@
+#ifndef HOMID_LOG_H
+#define HOMID_LOG_H
+
 #include <syslog.h>
 
 void
@@ -5,3 +8,5 @@ homid_log_set_level(int level);
 
 void
 homid_log(int level, const char *fmt, ...);
+
+#endif /* HOMID_LOG_H */
